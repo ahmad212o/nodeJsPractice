7455d6710d255e6598d443f8c9be794b3fcdb422
ahmad sh <ahmad.sharabati.96@gmail.com>
2021-01-25 00:58:22 +0200

    update2

--- App.js
+++ App.js
@@ -2,13 +2,33 @@ const fs=require('fs');
 const http=require('http');
 const path=require('path');
 const express = require('express')
+const multer=require('multer');
 const app=express();
+
+const fileStorage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'assets/images')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,new Date().toString()+'_'+file.originalname);
+    }
+
+});
+const fileFilter=(req,file,cb)=>{
+    if(file.mimetype==='image/png'||file.mimetype==='image/jpg'||file.mimetype==='image/jpeg'){
+        cb(null,true)
+    }else{
+        cb(null,false);
+    }
+}
+
 const mongoose = require('mongoose');
 //OBnDOmoWp6JJhtot
 const FeedRoutes=require('./Routers/Feed')
 const bodyParser=require('body-parser')
+app.use(multer({storage :fileStorage,fileFilter:fileFilter}).single('image'));
 app.use(bodyParser.json());
-app.use('/assets/images',express.static(path.join(__dirname)))
+app.use('/assets/images',express.static(path.join(__dirname,'assets/images')))
 app.use((req, res, next)=>{
 res.setHeader('Access-Control-Allow-Origin','*');
 res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH');
@@ -20,9 +40,7 @@ app.use((error,req,res,next)=>{
 console.log(error);
 const status=error.statusCode || 500;
 const message=error.message;
-res.status(status).json({message:message});
-
-})
+res.status(status).json({message:message});})
 mongoose.connect('mongodb+srv://ahmad:OBnDOmoWp6JJhtot@cluster0.qxtb7.mongodb.net/ahmad?retryWrites=true&w=majority').then(result=>{
     app.listen(3000);
 }).catch(err=>{
